d753e9f10381e16b4f69710616d62c6d7cc62507
Francis Hartojo <fhartojo@gmail.com>
2018-01-10 22:31:57 -0700

    Simplify tables and loading logic.

--- src/main/java/com/quiteharmless/azbcdb/Loader.java
+++ src/main/java/com/quiteharmless/azbcdb/Loader.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Types;
 import java.time.LocalDate;
 import java.time.MonthDay;
 import java.time.ZoneId;
@@ -27,16 +28,16 @@ public class Loader {
 		PreparedStatement selectMembershipTypeSql = null;
 		PreparedStatement deleteTableSql = null;
 		PreparedStatement replaceMemberSql = null;
-		PreparedStatement replaceFamilySql = null;
+//		PreparedStatement replaceFamilySql = null;
 		PreparedStatement replaceMembershipSql = null;
 		PreparedStatement replaceMembershipLookupSql = null;
 
 		try {
-			connection = DriverManager.getConnection("jdbc:sqlite:/home/francis/data/azbc.db");
+			connection = DriverManager.getConnection("jdbc:sqlite:/home/francis/data/azbcMember.db");
 			selectMembershipTypeSql = connection.prepareStatement("select mbrship_type_id, mbrship_type_nm, mbrship_len, mbrship_len_type_cd, mbrship_auto_renew, mbrship_max_visit from mbrship_type");
-			replaceMemberSql = connection.prepareStatement("insert or replace into member(mbr_id, first_nm, last_nm) values(?, ?, ?)");
-			replaceFamilySql = connection.prepareStatement("insert or replace into mbrship_fmly(mbr_id, mbr_hof_id) values(?, ?)");
-			replaceMembershipSql = connection.prepareStatement("insert or replace into mbr_mbrship(mbr_id, active_ind, mbrship_type_id, mbr_mbrship_start_dt, mbr_mbrship_end_dt) values(?, ?, ?, date(?, 'unixepoch'), date(?, 'unixepoch'))");
+			replaceMemberSql = connection.prepareStatement("insert or replace into mbr(mbr_id, first_nm, last_nm) values(?, ?, ?)");
+//			replaceFamilySql = connection.prepareStatement("insert or replace into mbrship_fmly(mbr_id, mbr_hof_id) values(?, ?)");
+			replaceMembershipSql = connection.prepareStatement("insert or replace into mbr_mbrship(mbr_id, active_ind, mbrship_type_id, mbr_mbrship_start_dt, mbr_mbrship_end_dt, mbr_hof_id) values(?, ?, ?, date(?, 'unixepoch'), date(?, 'unixepoch'), ?)");
 			replaceMembershipLookupSql = connection.prepareStatement("insert or replace into mbr_lu(mbr_id, mbr_lu_id) values(?, ?)");
 			membershipTypeMap = new HashMap<Integer, MembershipType>();
 			ResultSet rs = selectMembershipTypeSql.executeQuery();
@@ -69,10 +70,7 @@ public class Loader {
 				ZoneId zoneId = ZoneId.systemDefault();
 				LocalDate today = LocalDate.now(zoneId);
 
-				deleteTableSql = connection.prepareStatement("delete from member");
-				deleteTableSql.executeUpdate();
-				deleteTableSql.close();
-				deleteTableSql = connection.prepareStatement("delete from mbrship_fmly");
+				deleteTableSql = connection.prepareStatement("delete from mbr");
 				deleteTableSql.executeUpdate();
 				deleteTableSql.close();
 				deleteTableSql = connection.prepareStatement("delete from mbr_mbrship");
@@ -83,29 +81,24 @@ public class Loader {
 //				deleteTableSql.close();
 
 				for (int i = 1; i < membershipTable.getRowCount(); i++) {
-					boolean isFamily = false;
 					Row row = membershipTable.getRowByIndex(i);
-					Integer id = Integer.valueOf(row.getCellByIndex(0).getStringValue());
+					Long id = Long.valueOf(row.getCellByIndex(0).getStringValue());
 					String startDateString = row.getCellByIndex(1).getStringValue();
 					String firstName = StringUtils.trimToEmpty(row.getCellByIndex(2).getStringValue());
 					String lastName = StringUtils.trimToEmpty(row.getCellByIndex(3).getStringValue());
 					String type = row.getCellByIndex(4).getStringValue().toUpperCase();
 					String typeLength = row.getCellByIndex(5).getStringValue().toUpperCase();
 					Integer isCurrent = Integer.valueOf(row.getCellByIndex(6).getStringValue().toUpperCase().equals("Y") ? 1 : 0);
-					Integer hofId = null;
-					if (type.equals("FAM")) {
-						isFamily = true;
+					Long hofId = null;
+					boolean isFamily = false;
 
-						try {
-							hofId = Integer.valueOf(StringUtils.trimToNull(row.getCellByIndex(8).getStringValue()) != null ? row.getCellByIndex(8).getStringValue() : "0");
-						} catch (NumberFormatException e) {
-							hofId = Integer.valueOf(0);
-						}
+					try {
+						hofId = Long.valueOf(StringUtils.trimToNull(row.getCellByIndex(8).getStringValue()) != null ? row.getCellByIndex(8).getStringValue() : "0");
+					} catch (NumberFormatException e) {
+						hofId = Long.valueOf(0);
 					}
 
-					System.out.println(id + "," + firstName + "," + lastName + "," + type + "," + typeLength + "," + isCurrent + "," + hofId);
-
-					replaceMemberSql.setInt(1, id);
+					replaceMemberSql.setLong(1, id);
 					replaceMemberSql.setString(2, firstName);
 					replaceMemberSql.setString(3, lastName);
 					replaceMemberSql.executeUpdate();
@@ -114,26 +107,17 @@ public class Loader {
 //					replaceMembershipLookupSql.setString(2, "");
 //					replaceMembershipLookupSql.executeUpdate();
 
-					if (isFamily) {
-						if (hofId != 0) {
-							replaceFamilySql.setInt(1, id);
-							replaceFamilySql.setInt(2, hofId);
-							replaceFamilySql.executeUpdate();
-						}
-					}
-
 					Integer membershipTypeId = 0;
 					LocalDate startDate = LocalDate.parse(startDateString, formatter);
 					long elapsedMonths = ChronoUnit.MONTHS.between(startDate, today);
 					LocalDate endDate = MonthDay.from(startDate).atYear(today.getYear());
 
-					replaceMembershipSql.setInt(1, id);
-					replaceMembershipSql.setInt(2, isCurrent);
-
 					if (typeLength.equals("LIFE") || typeLength.equals("HON")) {
 						membershipTypeId = 1;
 						endDate = startDate.plusYears(membershipTypeMap.get(membershipTypeId).getLength());
 					} else if (type.equals("FAM")) {
+						isFamily = true;
+
 						if (typeLength.equals("ANN")) {
 							membershipTypeId = 2;
 							if (isCurrent == 1) {
@@ -208,12 +192,23 @@ public class Loader {
 							System.err.println("Unrecognised typeLength:  " + typeLength);
 						}
 					}
+
+					System.out.println(id + "," + firstName + "," + lastName + "," + type + "," + typeLength + "," + isCurrent + "," + hofId + "," + isFamily + "," + (id.equals(hofId)));
+
+					replaceMembershipSql.setLong(1, id);
 					replaceMembershipSql.setInt(3, membershipTypeId);
 					replaceMembershipSql.setLong(4, startDate.atStartOfDay(zoneId).toEpochSecond());
-					replaceMembershipSql.setLong(5, endDate.atStartOfDay(zoneId).toEpochSecond());
-					if (!isFamily || (isFamily && (id == hofId))) {
-						replaceMembershipSql.executeUpdate();
+					replaceMembershipSql.setLong(6, hofId);
+
+					if (!isFamily || (isFamily && (id.equals(hofId)))) {
+						replaceMembershipSql.setInt(2, isCurrent);
+						replaceMembershipSql.setLong(5, endDate.atStartOfDay(zoneId).toEpochSecond());
+					} else {
+						replaceMembershipSql.setObject(2, null, Types.INTEGER);
+						replaceMembershipSql.setObject(5, null, Types.BIGINT);
 					}
+
+					replaceMembershipSql.executeUpdate();
 				}
 			} else {
 				System.out.println(membershipDocFilename + " is not a readable spreadsheet");
@@ -232,10 +227,6 @@ public class Loader {
 
 				if (replaceMemberSql != null) {
 					replaceMemberSql.close();
-				}
-
-				if (replaceFamilySql != null) {
-					replaceFamilySql.close();
 				}
 
 				if (replaceMembershipSql != null) {
