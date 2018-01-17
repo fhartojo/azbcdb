98783b9dfa30e72f6ae782b4e372908b69eefd80
Francis Hartojo <fhartojo@gmail.com>
2018-01-17 00:12:47 -0700

    Changes based on the latest spreadsheet.

--- src/main/java/com/quiteharmless/azbcdb/Loader.java
+++ src/main/java/com/quiteharmless/azbcdb/Loader.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 import java.time.MonthDay;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,13 +21,21 @@ import org.odftoolkit.simple.table.Table;
 public class Loader {
 	private Map<Integer, MembershipType> membershipTypeMap;
 
+	private static final Long VISITOR_ID = new Long(99999L);
+
+	private static final String VISITOR_LOOKUP_ID = "v";
+
+	private static final String VISITOR_NAME = "Visitor";
+
+	private static final LocalDate VISITOR_START_DATE = LocalDate.of(2012, 1, 1);
+
 	private void loadData(String membershipDocFilename) {
 		SpreadsheetDocument membershipDoc = null;
 		Connection connection = null;
 		PreparedStatement selectMembershipTypeSql = null;
 		PreparedStatement deleteTableSql = null;
 		PreparedStatement replaceMemberSql = null;
-//		PreparedStatement replaceFamilySql = null;
+		PreparedStatement replaceNoteSql = null;
 		PreparedStatement replaceMembershipSql = null;
 		PreparedStatement replaceMembershipLookupSql = null;
 
@@ -36,11 +43,15 @@ public class Loader {
 			connection = DriverManager.getConnection("jdbc:sqlite:/home/francis/data/azbcMember.db");
 			selectMembershipTypeSql = connection.prepareStatement("select mbrship_type_id, mbrship_type_nm, mbrship_len, mbrship_len_type_cd, mbrship_auto_renew, mbrship_max_visit from mbrship_type");
 			replaceMemberSql = connection.prepareStatement("insert or replace into mbr(mbr_id, first_nm, last_nm) values(?, ?, ?)");
-//			replaceFamilySql = connection.prepareStatement("insert or replace into mbrship_fmly(mbr_id, mbr_hof_id) values(?, ?)");
+			replaceNoteSql = connection.prepareStatement("insert or replace into mbr_note(mbr_id, mbr_note_txt) values(?, ?)");
 			replaceMembershipSql = connection.prepareStatement("insert or replace into mbr_mbrship(mbr_id, active_ind, mbrship_type_id, mbr_mbrship_start_dt, mbr_mbrship_end_dt, mbr_hof_id) values(?, ?, ?, date(?, 'unixepoch'), date(?, 'unixepoch'), ?)");
 			replaceMembershipLookupSql = connection.prepareStatement("insert or replace into mbr_lu(mbr_id, mbr_lu_id) values(?, ?)");
 			membershipTypeMap = new HashMap<Integer, MembershipType>();
 			ResultSet rs = selectMembershipTypeSql.executeQuery();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy");
+			ZoneId zoneId = ZoneId.systemDefault();
+			LocalDate today = LocalDate.now(zoneId);
+			LocalDate infiniteDate = today.plusYears(100L);
 
 			while(rs.next()) {
 				MembershipType membershipType = new MembershipType();
@@ -66,14 +77,13 @@ public class Loader {
 
 				System.out.println("columnCount = " + membershipTable.getColumnCount() + "; rowCount = " + membershipTable.getRowCount());
 
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy");
-				ZoneId zoneId = ZoneId.systemDefault();
-				LocalDate today = LocalDate.now(zoneId);
-
 				deleteTableSql = connection.prepareStatement("delete from mbr");
 				deleteTableSql.executeUpdate();
 				deleteTableSql.close();
 				deleteTableSql = connection.prepareStatement("delete from mbr_mbrship");
+				deleteTableSql.executeUpdate();
+				deleteTableSql.close();
+				deleteTableSql = connection.prepareStatement("delete from mbr_note");
 				deleteTableSql.executeUpdate();
 				deleteTableSql.close();
 //				deleteTableSql = connection.prepareStatement("delete from mbr_lu");
@@ -82,136 +92,173 @@ public class Loader {
 
 				for (int i = 1; i < membershipTable.getRowCount(); i++) {
 					Row row = membershipTable.getRowByIndex(i);
-					Long id = Long.valueOf(row.getCellByIndex(0).getStringValue());
-					String startDateString = row.getCellByIndex(1).getStringValue();
-					String firstName = StringUtils.trimToEmpty(row.getCellByIndex(2).getStringValue());
-					String lastName = StringUtils.trimToEmpty(row.getCellByIndex(3).getStringValue());
-					String type = row.getCellByIndex(4).getStringValue().toUpperCase();
-					String typeLength = row.getCellByIndex(5).getStringValue().toUpperCase();
-					Integer isCurrent = Integer.valueOf(row.getCellByIndex(6).getStringValue().toUpperCase().equals("Y") ? 1 : 0);
-					Long hofId = null;
-					boolean isFamily = false;
+					if (StringUtils.isNotBlank(StringUtils.trimToEmpty(row.getCellByIndex(0).getStringValue()))) {
+						Long id = Long.valueOf(row.getCellByIndex(0).getStringValue());
+						String startDateString = row.getCellByIndex(1).getStringValue();
+						String firstName = StringUtils.trimToEmpty(row.getCellByIndex(2).getStringValue());
+						String lastName = StringUtils.trimToEmpty(row.getCellByIndex(3).getStringValue());
+						String type = StringUtils.trimToEmpty(row.getCellByIndex(4).getStringValue().toUpperCase());
+						String typeLength = StringUtils.trimToEmpty(row.getCellByIndex(5).getStringValue().toUpperCase());
+						Integer isCurrent = Integer.valueOf(row.getCellByIndex(6).getStringValue().toUpperCase().equals("Y") ? 1 : 0);
+						String endDateString = StringUtils.trimToNull(row.getCellByIndex(11).getStringValue());
+						String note = StringUtils.trimToNull(row.getCellByIndex(12).getStringValue());
+						Long hofId = null;
+						boolean isFamily = false;
 
-					try {
-						hofId = Long.valueOf(StringUtils.trimToNull(row.getCellByIndex(8).getStringValue()) != null ? row.getCellByIndex(8).getStringValue() : "0");
-					} catch (NumberFormatException e) {
-						hofId = Long.valueOf(0);
-					}
-
-					replaceMemberSql.setLong(1, id);
-					replaceMemberSql.setString(2, firstName);
-					replaceMemberSql.setString(3, lastName);
-					replaceMemberSql.executeUpdate();
-
-//					replaceMembershipLookupSql.setInt(1, id);
-//					replaceMembershipLookupSql.setString(2, "");
-//					replaceMembershipLookupSql.executeUpdate();
-
-					Integer membershipTypeId = 0;
-					LocalDate startDate = LocalDate.parse(startDateString, formatter);
-					long elapsedMonths = ChronoUnit.MONTHS.between(startDate, today);
-					LocalDate endDate = MonthDay.from(startDate).atYear(today.getYear());
-
-					if (typeLength.equals("LIFE") || typeLength.equals("HON")) {
-						membershipTypeId = 1;
-						endDate = startDate.plusYears(membershipTypeMap.get(membershipTypeId).getLength());
-					} else if (type.equals("FAM")) {
-						isFamily = true;
-
-						if (typeLength.equals("ANN")) {
-							membershipTypeId = 2;
-							if (isCurrent == 1) {
-								if (!endDate.isAfter(today)) {
-									endDate = endDate.plusYears(1);
-								}
-							} else {
-								if (elapsedMonths > 12) {
-									endDate = startDate.plusMonths(12);
-								}
-							}
-						} else if (typeLength.equals("MON")) {
-							membershipTypeId = 4;
-							if (isCurrent == 1) {
-								if (!endDate.isAfter(today)) {
-									endDate = MonthDay.from(startDate).with(today.getMonth()).atYear(today.getYear()).plusMonths(1);
-								}
-							} else {
-								if (elapsedMonths > 2) {
-									endDate = startDate.plusMonths(2);
-								}
-							}
-						} else if (typeLength.equals("SHT")) {
-							membershipTypeId = 6;
-							if (isCurrent == 1) {
-								if (!endDate.isAfter(today)) {
-									endDate = MonthDay.from(startDate).with(today.getMonth()).atYear(today.getYear()).plusMonths(1);
-								}
-							} else {
-								if (elapsedMonths > 1) {
-									endDate = startDate.plusMonths(1);
-								}
-							}
-						} else {
-							System.err.println("Unrecognised typeLength:  " + typeLength);
+						try {
+							hofId = Long.valueOf(StringUtils.trimToNull(row.getCellByIndex(8).getStringValue()) != null ? row.getCellByIndex(8).getStringValue() : "0");
+						} catch (NumberFormatException e) {
+							hofId = Long.valueOf(0);
 						}
-					} else if (type.equals("IND")) {
-						if (typeLength.equals("ANN")) {
-							membershipTypeId = 3;
-							if (isCurrent == 1) {
-								if (!endDate.isAfter(today)) {
-									endDate = endDate.plusYears(1);
+
+						replaceMemberSql.setLong(1, id);
+						replaceMemberSql.setString(2, firstName);
+						replaceMemberSql.setString(3, lastName);
+						replaceMemberSql.executeUpdate();
+
+//						replaceMembershipLookupSql.setInt(1, id);
+//						replaceMembershipLookupSql.setString(2, "");
+//						replaceMembershipLookupSql.executeUpdate();
+
+						Integer membershipTypeId = 0;
+						LocalDate startDate = LocalDate.parse(startDateString, formatter);
+						LocalDate endDate = MonthDay.from(startDate).atYear(today.getYear());
+
+						if (typeLength.equals("LIFE") || typeLength.equals("HON")) {
+							membershipTypeId = 1;
+							endDate = infiniteDate;
+						} else if (type.equals("FAM")) {
+							isFamily = true;
+
+							if (typeLength.equals("ANN")) {
+								membershipTypeId = 2;
+								if (StringUtils.isNotBlank(endDateString)) {
+									endDate = LocalDate.parse(endDateString, formatter);
+								} else {
+									if (isCurrent.intValue() == 1) {
+										System.err.println("endDateString is blank");
+
+										endDate = null;
+									} else {
+										endDate = startDate.plusYears(1L);
+									}
+								}
+							} else if (typeLength.equals("MON")) {
+								membershipTypeId = 4;
+								if (StringUtils.isNotBlank(endDateString)) {
+									endDate = LocalDate.parse(endDateString, formatter);
+								} else {
+									if (isCurrent.intValue() == 1) {
+										endDate = infiniteDate;
+									} else {
+										endDate = startDate.plusMonths(1L);
+									}
+								}
+							} else if (typeLength.equals("SHT")) {
+								membershipTypeId = 6;
+								if (StringUtils.isNotBlank(endDateString)) {
+									endDate = LocalDate.parse(endDateString, formatter);
+								} else {
+									if (isCurrent.intValue() == 1) {
+										System.err.println("endDateString is blank");
+
+										endDate = null;
+									} else {
+										endDate = startDate.plusMonths(1L);
+									}
 								}
 							} else {
-								if (elapsedMonths > 12) {
-									endDate = startDate.plusMonths(12);
-								}
+								System.err.println("Unrecognised typeLength:  " + typeLength);
 							}
-						} else if (typeLength.equals("MON")) {
-							membershipTypeId = 5;
-							if (isCurrent == 1) {
-								if (!endDate.isAfter(today)) {
-									endDate = MonthDay.from(startDate).with(today.getMonth()).atYear(today.getYear()).plusMonths(1);
+						} else if (type.equals("IND")) {
+							if (typeLength.equals("ANN")) {
+								membershipTypeId = 3;
+								if (StringUtils.isNotBlank(endDateString)) {
+									endDate = LocalDate.parse(endDateString, formatter);
+								} else {
+									if (isCurrent.intValue() == 1) {
+										System.err.println("endDateString is blank");
+
+										endDate = null;
+									} else {
+										endDate = startDate.plusYears(1L);
+									}
+								}
+							} else if (typeLength.equals("MON")) {
+								membershipTypeId = 5;
+								if (StringUtils.isNotBlank(endDateString)) {
+									endDate = LocalDate.parse(endDateString, formatter);
+								} else {
+									if (isCurrent.intValue() == 1) {
+										endDate = infiniteDate;
+									} else {
+										endDate = startDate.plusMonths(1L);
+									}
+								}
+							} else if (typeLength.equals("SHT")) {
+								membershipTypeId = 7;
+								if (StringUtils.isNotBlank(endDateString)) {
+									endDate = LocalDate.parse(endDateString, formatter);
+								} else {
+									if (isCurrent.intValue() == 1) {
+										System.err.println("endDateString is blank");
+
+										endDate = null;
+									} else {
+										endDate = startDate.plusMonths(1L);
+									}
 								}
 							} else {
-								if (elapsedMonths > 2) {
-									endDate = startDate.plusMonths(2);
-								}
+								System.err.println("Unrecognised typeLength:  " + typeLength);
 							}
-						} else if (typeLength.equals("SHT")) {
-							membershipTypeId = 7;
-							if (isCurrent == 1) {
-								if (!endDate.isAfter(today)) {
-									endDate = MonthDay.from(startDate).with(today.getMonth()).atYear(today.getYear()).plusMonths(1);
-								}
-							} else {
-								if (elapsedMonths > 1) {
-									endDate = startDate.plusMonths(1);
-								}
-							}
-						} else {
-							System.err.println("Unrecognised typeLength:  " + typeLength);
 						}
-					}
 
-					System.out.println(id + "," + firstName + "," + lastName + "," + type + "," + typeLength + "," + isCurrent + "," + hofId + "," + isFamily + "," + (id.equals(hofId)));
+						System.out.println(id + "," + firstName + "," + lastName + "," + type + "," + typeLength + "," + isCurrent + "," + hofId + "," + isFamily + "," + (id.equals(hofId)));
 
-					replaceMembershipSql.setLong(1, id);
-					replaceMembershipSql.setInt(3, membershipTypeId);
-					replaceMembershipSql.setLong(4, startDate.atStartOfDay(zoneId).toEpochSecond());
-					replaceMembershipSql.setLong(6, hofId);
+						replaceMembershipSql.setLong(1, id);
+						replaceMembershipSql.setInt(3, membershipTypeId);
+						replaceMembershipSql.setLong(4, startDate.atStartOfDay(zoneId).toEpochSecond());
+						replaceMembershipSql.setLong(6, hofId);
 
-					if (!isFamily || (isFamily && (id.equals(hofId)))) {
-						replaceMembershipSql.setInt(2, isCurrent);
-						replaceMembershipSql.setLong(5, endDate.atStartOfDay(zoneId).toEpochSecond());
+						if (!isFamily || (isFamily && (id.equals(hofId)))) {
+							replaceMembershipSql.setInt(2, isCurrent);
+							replaceMembershipSql.setLong(5, endDate.atStartOfDay(zoneId).toEpochSecond());
+						} else {
+							replaceMembershipSql.setObject(2, null, Types.INTEGER);
+							replaceMembershipSql.setObject(5, null, Types.BIGINT);
+						}
+
+						replaceMembershipSql.executeUpdate();
+
+						if (StringUtils.isNotBlank(note)) {
+							replaceNoteSql.setLong(1, id);
+							replaceNoteSql.setString(2, note);
+
+							replaceNoteSql.executeUpdate();
+						}
 					} else {
-						replaceMembershipSql.setObject(2, null, Types.INTEGER);
-						replaceMembershipSql.setObject(5, null, Types.BIGINT);
+						break;
 					}
-
-					replaceMembershipSql.executeUpdate();
 				}
+
+				replaceMemberSql.setLong(1, VISITOR_ID);
+				replaceMemberSql.setString(2, VISITOR_NAME);
+				replaceMemberSql.setString(3, VISITOR_NAME);
+				replaceMemberSql.executeUpdate();
+
+				replaceMembershipSql.setLong(1, VISITOR_ID);
+				replaceMembershipSql.setInt(2, 1);
+				replaceMembershipSql.setInt(3, 1);
+				replaceMembershipSql.setLong(4, VISITOR_START_DATE.atStartOfDay(zoneId).toEpochSecond());
+				replaceMembershipSql.setLong(5, infiniteDate.atStartOfDay(zoneId).toEpochSecond());
+				replaceMembershipSql.setLong(6, Long.valueOf(0));
+				replaceMembershipSql.executeUpdate();
+
+				replaceMembershipLookupSql.setLong(1, VISITOR_ID);
+				replaceMembershipLookupSql.setString(2, VISITOR_LOOKUP_ID);
+				replaceMembershipLookupSql.executeUpdate();
 			} else {
-				System.out.println(membershipDocFilename + " is not a readable spreadsheet");
+				System.err.println(membershipDocFilename + " is not a readable spreadsheet");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -231,6 +278,10 @@ public class Loader {
 
 				if (replaceMembershipSql != null) {
 					replaceMembershipSql.close();
+				}
+
+				if (replaceNoteSql != null) {
+					replaceNoteSql.close();
 				}
 
 				if (replaceMembershipLookupSql != null) {
